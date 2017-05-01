65464b6b2029e5dcc2c2dc98667a56dbf9fff4d9
Binh Van Nguyen <binhnv80@gmail.com>
2017-05-01 11:53:05 -0700

    Add echo message when downloading package and link hive-site.xml to Hadoop conf dir

--- scripts/build/install.sh
+++ scripts/build/install.sh
@@ -5,8 +5,10 @@ set -e
 g_hive_user="${HIVE_USER}"
 g_hive_home="${HIVE_HOME}"
 g_log_dir="${HIVE_LOG_DIR}"
+g_hive_conf_dir="${HIVE_CONF_DIR}"
+g_hadoop_conf_dir="${HADOOP_CONF_DIR}"
 g_mysql_connector_jar=${MYSQL_CONNECTOR_JAR}
-g_hive_url="http://www.apache.org/dist/hive/hive-${HIVE_VERSION}/apache-hive-${HIVE_VERSION}-bin.tar.gz"
+g_hive_url="http://archive.apache.org/dist/hive/hive-${HIVE_VERSION}/apache-hive-${HIVE_VERSION}-bin.tar.gz"
 
 function install_dependencies {
     apt-get update
@@ -19,10 +21,12 @@ function install_package {
     install_dependencies
 
     mkdir -p ${g_hive_home}
+    echo "Downloading ${g_hive_url}..."
     curl -L ${g_hive_url} | tar -xzC ${g_hive_home} --strip-components=1
     chown -R ${g_hive_user} ${g_hive_home}
 
     ln -s ${g_mysql_connector_jar} ${g_hive_home}/lib/mysql-connector-java.jar
+    ln -s ${g_hive_conf_dir}/hive-site.xml ${g_hadoop_conf_dir}/hive-site.xml
 }
 
 function create_user {
